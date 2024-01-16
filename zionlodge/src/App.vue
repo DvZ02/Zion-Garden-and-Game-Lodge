708b43fd2e0d0b6779075cc9da63bb495ecca3a3
DvZ <danielvzyl1469@gmail.com>
2024-01-16 17:00:48 +0200

    💄 Navbar added

--- zionlodge/src/App.vue
+++ zionlodge/src/App.vue
@@ -1,6 +1,29 @@
 <template>
  <div id='app'>
-    Hello World!
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img id="nav-logo" src="./assets/logo.svg" alt="Logo" width="70%" height="auto" class="d-inline-block align-text-top">
+        </a>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ACCOMODATION</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ABOUT US</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CONTACT US</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">GALLERY</a>
+          </li>
+        </ul>
+      </div>
+  </nav>
  </div>
 </template>
 
