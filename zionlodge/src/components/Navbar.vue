58986db19c351f1d9bed815742b38677d936f525
DvZ <danielvzyl1469@gmail.com>
2024-01-17 22:25:14 +0200

    💄 Mobile Navbar done

--- zionlodge/src/components/Navbar.vue
+++ zionlodge/src/components/Navbar.vue
@@ -2,9 +2,9 @@
   <header class="bg-white">
     <nav class="flex justify-between items-center w-[92%] mx-auto">
       <div>
-        <img class="w-36" id="nav-logo" src="../assets/logo.svg" alt="...">
+        <img class="w-40" id="nav-logo" src="../assets/logo.svg" alt="...">
       </div>
-      <div class="md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
+      <div ref="navlinks" class="duration-500 md:static absolute bg-white md:min-h-fit min-h-[20vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
         <ul class="flex md:flex-row flex-col md:items-center md:gap-[6vw] gap-8">
             <li>
               <a class="hover:text-gray-500" href="#">HOME</a>
@@ -24,41 +24,27 @@
           </ul>
       </div>
       <div class="flex items-center gap-6">
-        <ion-icon onclick="toggleMenu(this)" name="menu" class="text-3xl cursor-pointer md:hidden"></ion-icon>
+        <ion-icon ref="menuicon" @click="toggleMenu()" name="menu" class="text-3xl cursor-pointer md:hidden"></ion-icon>
       </div>
     </nav>
   </header>
 </template>
 
-<script>
+<script setup>
+  import { ref } from 'vue';
 
-function toggleMenu(e){
-  e.name = e.name === 'menu' ? 'close' : 'menu';
-}
+  const navlinks = ref(null) ;
+  const menuicon = ref(null) ;
 
-export default {
-  name: 'NavBar',
-}
+  const toggleMenu = () => {
+    menuicon.value.name = menuicon.value.name === 'menu' ? 'close' : 'menu';
+    navlinks.value.classList.toggle('top-[9%]');
+  }
+
 </script>
 
-<!-- <style>
-  .navbar-brand{
-    margin-left: 5%;  
-    width: 20%;
+<script>
+  export default {
+    name: 'NavBar',
   }
-
-
-  .navbar-nav{
-    display: flex;
-    margin-left: auto;
-    margin-right: auto;
-    width: 100%;
-  }
-
-
- .nav-item {
-  /* width: 100%; */
-  font-size: xx-large;
-  margin-left: 5%;
- }
-</style> -->
+</script>
