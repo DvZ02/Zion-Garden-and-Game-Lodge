e9c804994908ce0325f22aeb0acc1279bc0feb78
DvZ <danielvzyl1469@gmail.com>
2024-01-17 17:45:33 +0200

    🎨 Created Navbar as component

--- zionlodge/src/App.vue
+++ zionlodge/src/App.vue
@@ -1,33 +1,9 @@
 <template>
- <div id='app'>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img id="nav-logo" src="./assets/logo.svg" alt="Logo" width="70%" height="auto" class="d-inline-block align-text-top">
-        </a>
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">HOME</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">ACCOMODATION</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">ABOUT US</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">CONTACT US</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">GALLERY</a>
-          </li>
-        </ul>
-      </div>
-  </nav>
- </div>
+  <NavBar/>
 </template>
 
 <script setup>
+  import NavBar from './components/Navbar.vue'
   import { onMounted, ref } from 'vue';
   import { db } from './firebase';
   import { collection, doc, getDocs } from 'firebase/firestore';
@@ -41,25 +17,3 @@
     });
   })
 </script>
-
-<style>
-  .navbar-brand{
-    margin-left: 5%;  
-    width: 20%;
-  }
-
-
-  .navbar-nav{
-    display: flex;
-    margin-left: auto;
-    margin-right: auto;
-    width: 100%;
-  }
-
-
- .nav-item {
-  /* width: 100%; */
-  font-size: xx-large;
-  margin-left: 5%;
- }
-</style>
---
+++ zionlodge/src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img id="nav-logo" src="../assets/logo.svg" alt="Logo" width="70%" height="auto" class="d-inline-block align-text-top">
+        </a>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ACCOMODATION</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ABOUT US</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CONTACT US</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">GALLERY</a>
+          </li>
+        </ul>
+      </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
+
+<style>
+  .navbar-brand{
+    margin-left: 5%;  
+    width: 20%;
+  }
+
+
+  .navbar-nav{
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+  }
+
+
+ .nav-item {
+  /* width: 100%; */
+  font-size: xx-large;
+  margin-left: 5%;
+ }
+</style>
