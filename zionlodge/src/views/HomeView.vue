0fc5beb372d722e2e93763699b257bd7fab6b019
DvZ <danielvzyl1469@gmail.com>
2024-01-18 13:08:20 +0200

    💄 Home image carousel

--- zionlodge/src/views/HomeView.vue
+++ zionlodge/src/views/HomeView.vue
@@ -5,7 +5,10 @@
 
     <div class="flex items-center justify-center">
         <div class="bg-ziongreen w-[92%] md:h-[40svh] h-[35svh] shadow-xl rounded-2xl p-6">
-            
+            <img class="md:w-[30%] rounded-[7%] mx-auto" id="nav-logo" src="../assets/lugfoto.jpg" alt="...">
+            <p class="font-title mx-auto text-center p-12 text-xl">
+                “For the LORD has chosen Zion, he has desired it for his dwelling, saying, “This is my resting place for ever and ever; here I will sit enthroned, for I have desired it.” Psalm 132:13
+            </p>
         </div>
     </div>
 </template>
