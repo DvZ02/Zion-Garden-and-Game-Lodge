d1647ede3b1798c4dc62b073209b91bf90c2754b
DvZ <danielvzyl1469@gmail.com>
2024-01-18 13:17:17 +0200

    📱 Home Banner responsiveness

--- zionlodge/src/views/HomeView.vue
+++ zionlodge/src/views/HomeView.vue
@@ -4,9 +4,9 @@
     </div>
 
     <div class="flex items-center justify-center">
-        <div class="bg-ziongreen w-[92%] md:h-[42svh] h-[35svh] shadow-xl rounded-2xl p-6">
-            <img class="md:h-[30svh] rounded-[7%] mx-auto" id="nav-logo" src="../assets/lugfoto.jpg" alt="...">
-            <p class="font-title mx-auto text-center p-6 text-xl">
+        <div class="bg-ziongreen w-[92%] md:h-[42svh] h-[40svh] shadow-xl rounded-2xl p-6">
+            <img class="md:h-[30svh] h-[23svh] rounded-[7%] mx-auto" id="nav-logo" src="../assets/lugfoto.jpg" alt="...">
+            <p class="font-title mx-auto text-center p-6 md:text-xl text-[8px]">
                 “For the LORD has chosen Zion, he has desired it for his dwelling, saying, <br/> This is my resting place for ever and ever; here I will sit enthroned, for I have desired it.” Psalm 132:13
             </p>
         </div>
