c32406800fcad09e3ab5b23ecb5b3512ca026b93
DvZ <danielvzyl1469@gmail.com>
2024-01-18 13:40:12 +0200

    🎨 Contact Us component

---
+++ zionlodge/src/components/ContactUs.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Hello
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+</script>
+
+<script>
+    export default {
+        name: 'ContactUs',
+    }
+</script>
