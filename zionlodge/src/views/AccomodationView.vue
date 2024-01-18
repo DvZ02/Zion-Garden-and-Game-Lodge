9d39a583b5b29352745026bc651d48cc9a9cee4c
DvZ <danielvzyl1469@gmail.com>
2024-01-18 22:56:08 +0200

    💄 Card title change

--- zionlodge/src/views/AccomodationView.vue
+++ zionlodge/src/views/AccomodationView.vue
@@ -3,13 +3,13 @@
     <div class="container mb-12">
         <div class="row -mx-[16%]">
             <div class="col">
-                <CardComponent title="Lapa" image="roos_1.png" 
+                <CardComponent title="Communal Kitchen & Lounge" image="roos_1.png" 
                     text="The open kitchen overlooks the beautiful garden and bushveld and is fully equipped with all kitchen utensils, cutlery, and crockery for 20 guests,
                     fridges, a 3-plate gas stove and a patio gas braai.  The lounge area as spacious with a great outdoor feeling to it. 
                     The furniture gives a wonderful earthly feeling of leather and food and this area allows for all guests to cook, visit and eat together."/>
             </div>
             <div class="col">
-                <CardComponent title="Lapa" image="roos_1.png" :colour="false" 
+                <CardComponent title="Sunset Deck" image="roos_1.png" :colour="false" 
                 text="The open kitchen overlooks the beautiful garden and bushveld and is fully equipped with all kitchen utensils, cutlery, and crockery for 20 guests,
                 fridges, a 3-plate gas stove and a patio gas braai.  The lounge area as spacious with a great outdoor feeling to it. 
                 The furniture gives a wonderful earthly feeling of leather and food and this area allows for all guests to cook, visit and eat together."/>                
@@ -17,13 +17,13 @@
         </div>
         <div class="row -mx-[16%]">
             <div class="col">
-                <CardComponent title="Lapa" image="roos_1.png" :allignment="false"
+                <CardComponent title="Boma" image="roos_1.png" :allignment="false"
                 text="The open kitchen overlooks the beautiful garden and bushveld and is fully equipped with all kitchen utensils, cutlery, and crockery for 20 guests,
                 fridges, a 3-plate gas stove and a patio gas braai.  The lounge area as spacious with a great outdoor feeling to it. 
                 The furniture gives a wonderful earthly feeling of leather and food and this area allows for all guests to cook, visit and eat together."/>
             </div>
             <div class="col">
-                <CardComponent title="Lapa" image="roos_1.png" :colour="false" :allignment="false"
+                <CardComponent title="Games & Conference Lapa" image="roos_1.png" :colour="false" :allignment="false"
                 text="The open kitchen overlooks the beautiful garden and bushveld and is fully equipped with all kitchen utensils, cutlery, and crockery for 20 guests,
                 fridges, a 3-plate gas stove and a patio gas braai.  The lounge area as spacious with a great outdoor feeling to it. 
                 The furniture gives a wonderful earthly feeling of leather and food and this area allows for all guests to cook, visit and eat together."/>
