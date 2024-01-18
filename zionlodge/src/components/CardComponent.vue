ac1f099dcc00c7f94670fc95d6d0d1cbd39594e4
DvZ <danielvzyl1469@gmail.com>
2024-01-18 19:18:09 +0200

    🚧 Card component iamge prop

--- zionlodge/src/components/CardComponent.vue
+++ zionlodge/src/components/CardComponent.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="flex items-center justify-center p-10 bg-ziongreen my-4 md:w-[45%] md:mx-5 shadow-xl rounded-2xl">
         <h1 class="md:text-[40px] text-[2svh] items-center font-title">{{ title }}</h1>
-        <img ref="cardimage" class="md:w-[15svw] rounded-[7%] mx-auto mt-12" src="../assets/sunset_1.jpg" alt="...">
+        <img class="md:w-[15svw] rounded-[7%] mx-auto mt-12" :src="require(`@/assets/${image}`)" alt="...">
         <p>{{ text }}</p>
     </div>
 </template>
 
 <script setup>
     import { ref } from 'vue';
-    const cardimage = ref(null);
 
     defineProps({
         title:{ 
