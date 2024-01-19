d225a8028112e899ede2175df224af42f583833f
DvZ <danielvzyl1469@gmail.com>
2024-01-19 22:27:44 +0200

    📱 Image banner responisveness

--- zionlodge/src/components/BannerComponent.vue
+++ zionlodge/src/components/BannerComponent.vue
@@ -1,9 +1,9 @@
 <template>
-            <div class="bg-ziongreen md:w-[85%] md:h-[44%] h-[40svh] shadow-xl rounded-2xl p-6 pt-12 overflow-hidden">
+            <div class="bg-ziongreen md:w-[85%] md:h-[44%] h-[28svh] w-[90%] shadow-xl rounded-2xl p-6 md:pt-12 pt-8 overflow-hidden">
             <!-- Chaos Starts Here -->
             <div class="flex items-center justify-center max-w-6xl mx-auto px-6">
-                <div class="flex [&:hover>div]:w-[5svw] [&>div:hover]:w-[50svw]">
-                    <div class="group relative h-[30svh] w-[3svw] overflow-hidden cursor-pointer transition-all duration-200">
+                <div class="flex md:[&:hover>div]:w-[5svw] md:[&>div:hover]:w-[20svw] [&:hover>div]:w-[3svw] [&>div:hover]:w-[45svw]">
+                    <div class="group relative md:h-[30svh] md:w-[5svw] h-[15svh] w-[3svw] overflow-hidden cursor-pointer transition-all duration-200">
                         <img class="h-full object-cover"  src="../assets/wildebees.jpg" alt="...">
                         <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
                             <div class="absolute inset-x-5 bottom-6">
@@ -15,7 +15,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="group relative h-[30svh] w-[5svw] overflow-hidden cursor-pointer transition-all duration-200">
+                    <div class="group relative md:h-[30svh] md:w-[5svw] h-[15svh] w-[3svw] overflow-hidden cursor-pointer transition-all duration-200">
                         <img class="h-full object-cover"  src="../assets/tuin_2.png" alt="...">
                         <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
                             <div class="absolute inset-x-5 bottom-6">
@@ -27,7 +27,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="group relative h-[30svh] w-[5svw] overflow-hidden cursor-pointer transition-all duration-200">
+                    <div class="group relative md:h-[30svh] md:w-[5svw] h-[15svh] w-[3svw] overflow-hidden cursor-pointer transition-all duration-200">
                         <img class="h-full object-cover"  src="../assets/tuin_tafel.png" alt="...">
                         <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
                             <div class="absolute inset-x-5 bottom-6">
@@ -39,7 +39,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="group relative h-[30svh] w-[5svw] overflow-hidden cursor-pointer transition-all duration-200">
+                    <div class="group relative md:h-[30svh] md:w-[5svw] h-[15svh] w-[3svw] overflow-hidden cursor-pointer transition-all duration-200">
                         <img class="h-full object-cover "  src="../assets/sunset_3.jpg" alt="...">
                         <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
                             <div class="absolute inset-x-5 bottom-6">
@@ -51,7 +51,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="group relative h-[30svh] w-[5svw] overflow-hidden cursor-pointer transition-all duration-200">
+                    <div class="group relative md:h-[30svh] md:w-[5svw] h-[15svh] w-[3svw] overflow-hidden cursor-pointer transition-all duration-200">
                         <img class="h-full object-cover "  src="../assets/eland.jpg" alt="...">
                         <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
                             <div class="absolute inset-x-5 bottom-6">
@@ -63,7 +63,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="group relative h-[30svh] w-[5svw] overflow-hidden cursor-pointer transition-all duration-200">
+                    <div class="group relative md:h-[30svh] md:w-[5svw] h-[15svh] w-[3svw] overflow-hidden cursor-pointer transition-all duration-200">
                         <img class="h-full object-cover "  src="../assets/love.jpg" alt="...">
                         <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
                             <div class="absolute inset-x-5 bottom-6">
@@ -75,7 +75,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="group relative h-[30svh] w-[50svw] overflow-hidden cursor-pointer transition-all duration-200">
+                    <div class="group relative md:h-[30svh] md:w-[20svw] h-[15svh] w-[45svw] overflow-hidden cursor-pointer transition-all duration-200">
                         <img class="h-full object-cover "  src="../assets/lugfoto_2.jpg" alt="...">
                         <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
                             <div class="absolute inset-x-5 bottom-6">
@@ -87,7 +87,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="group relative h-[30svh] w-[5svw] overflow-hidden cursor-pointer transition-all duration-200">
+                    <div class="group relative md:h-[30svh] md:w-[5svw] h-[15svh] w-[3svw] overflow-hidden cursor-pointer transition-all duration-200">
                         <img class="h-full object-cover "  src="../assets/game-drive.jpg" alt="...">
                         <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
                             <div class="absolute inset-x-5 bottom-6">
@@ -99,7 +99,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="group relative h-[30svh] w-[5svw] overflow-hidden cursor-pointer transition-all duration-200">
+                    <div class="group relative md:h-[30svh] md:w-[5svw] h-[15svh] w-[3svw] overflow-hidden cursor-pointer transition-all duration-200">
                         <img class="h-full object-cover "  src="../assets/kameelperd.jpg" alt="...">
                         <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
                             <div class="absolute inset-x-5 bottom-6">
@@ -111,7 +111,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="group relative h-[30svh] w-[5svw] overflow-hidden cursor-pointer transition-all duration-200">
+                    <div class="group relative md:h-[30svh] md:w-[5svw] h-[15svh] w-[3svw] overflow-hidden cursor-pointer transition-all duration-200">
                         <img class="h-full object-cover "  src="../assets/sunset_4.jpg" alt="...">
                         <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
                             <div class="absolute inset-x-5 bottom-6">
@@ -123,7 +123,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="group relative h-[30svh] w-[5svw] overflow-hidden cursor-pointer transition-all duration-200">
+                    <div class="group relative md:h-[30svh] md:w-[5svw] h-[15svh] w-[3svw] overflow-hidden cursor-pointer transition-all duration-200">
                         <img class="h-full object-cover"  src="../assets/swembad.png" alt="...">
                         <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
                             <div class="absolute inset-x-5 bottom-6">
@@ -135,7 +135,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="group relative h-[30svh] w-[5svw] overflow-hidden cursor-pointer transition-all duration-200">
+                    <div class="group relative md:h-[30svh] md:w-[5svw] h-[15svh] w-[3svw] overflow-hidden cursor-pointer transition-all duration-200">
                         <img class="h-full object-cover"  src="../assets/tuin_1.png" alt="...">
                         <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
                             <div class="absolute inset-x-5 bottom-6">
@@ -147,7 +147,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="group relative h-[30svh] w-[5svw] overflow-hidden cursor-pointer transition-all duration-200">
+                    <div class="group relative md:h-[30svh] md:w-[5svw] h-[15svh] w-[3svw] overflow-hidden cursor-pointer transition-all duration-200">
                         <img class="h-full object-cover"  src="../assets/zebra.jpg" alt="...">
                         <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
                             <div class="absolute inset-x-5 bottom-6">
@@ -162,7 +162,7 @@
                 </div>
             </div>
             <!-- <img class="md:h-[30svh] h-[23svh] rounde-[7%] mx-auto rounded-[7%]"  src="../assets/lugfoto.jpg" alt="..."> -->
-            <p class="font-title mx-auto text-center mt-2 p-6 md:text-xl text-[8px]">
+            <p class="font-title mx-auto text-center md:mt-2 -mt-2 md:p-6 p-3 md:text-xl text-[1svh]">
                 “For the LORD has chosen Zion, he has desired it for his dwelling, saying, <br/> This is my resting place for ever and ever; here I will sit enthroned, for I have desired it.” Psalm 132:13
             </p>
         </div>
