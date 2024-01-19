b88cc7f6d21f9722a1282d64632eb6c04ddc3ae3
DvZ <danielvzyl1469@gmail.com>
2024-01-19 15:05:27 +0200

    💄 Home Banner images

--- zionlodge/src/views/HomeView.vue
+++ zionlodge/src/views/HomeView.vue
@@ -2,9 +2,170 @@
     <TitleComponent title="Zion Garden & Game Lodge"/>
 
     <div class="flex items-center justify-center">
-        <div class="bg-ziongreen w-[92%] md:h-[42svh] h-[40svh] shadow-xl rounded-2xl p-6">
-            <img class="md:h-[30svh] h-[23svh] rounded-[7%] mx-auto" id="nav-logo" src="../assets/lugfoto.jpg" alt="...">
-            <p class="font-title mx-auto text-center p-6 md:text-xl text-[8px]">
+        <div class="bg-ziongreen w-[92%] md:h-[44%] h-[40svh] shadow-xl rounded-2xl p-6">
+            <!-- Chaos Starts Here -->
+            <div class="flex items-center justify-center max-w-6xl mx-auto px-6">
+                <div class="flex [&:hover>div]:w-16 [&>div:hover]:w-[30rem]">
+                    <div class="group relative h-96 w-16 overflow-hidden cursor-pointer transition-all duration-200">
+                        <img class="h-full object-cover"  src="../assets/wildebees.jpg" alt="...">
+                        <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
+                            <div class="absolute inset-x-5 bottom-6">
+                                <div class="flex gap-3 text-white">
+                                    <div>
+                                        <p class="font-semibold text-xl text-gray-100">Zion Garden & Game Lodge</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="group relative h-96 w-16 overflow-hidden cursor-pointer transition-all duration-200">
+                        <img class="h-full object-cover"  src="../assets/tuin_2.png" alt="...">
+                        <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
+                            <div class="absolute inset-x-5 bottom-6">
+                                <div class="flex gap-3 text-white">
+                                    <div>
+                                        <p class="font-semibold text-xl text-gray-100">Zion Garden & Game Lodge</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="group relative h-96 w-16 overflow-hidden cursor-pointer transition-all duration-200">
+                        <img class="h-full object-cover"  src="../assets/tuin_tafel.png" alt="...">
+                        <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
+                            <div class="absolute inset-x-5 bottom-6">
+                                <div class="flex gap-3 text-white">
+                                    <div>
+                                        <p class="font-semibold text-xl text-gray-100">Zion Garden & Game Lodge</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="group relative h-96 w-16 overflow-hidden cursor-pointer transition-all duration-200">
+                        <img class="h-full object-cover "  src="../assets/sunset_3.jpg" alt="...">
+                        <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
+                            <div class="absolute inset-x-5 bottom-6">
+                                <div class="flex gap-3 text-white">
+                                    <div>
+                                        <p class="font-semibold text-xl text-gray-100">Zion Garden & Game Lodge</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="group relative h-96 w-16 overflow-hidden cursor-pointer transition-all duration-200">
+                        <img class="h-full object-cover "  src="../assets/eland.jpg" alt="...">
+                        <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
+                            <div class="absolute inset-x-5 bottom-6">
+                                <div class="flex gap-3 text-white">
+                                    <div>
+                                        <p class="font-semibold text-xl text-gray-100">Zion Garden & Game Lodge</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="group relative h-96 w-16 overflow-hidden cursor-pointer transition-all duration-200">
+                        <img class="h-full object-cover "  src="../assets/love.jpg" alt="...">
+                        <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
+                            <div class="absolute inset-x-5 bottom-6">
+                                <div class="flex gap-3 text-white">
+                                    <div>
+                                        <p class="font-semibold text-xl text-gray-100">Zion Garden & Game Lodge</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="group relative h-96 w-[30rem] overflow-hidden cursor-pointer transition-all duration-200">
+                        <img class="h-full object-cover "  src="../assets/lugfoto_2.jpg" alt="...">
+                        <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
+                            <div class="absolute inset-x-5 bottom-6">
+                                <div class="flex gap-3 text-white">
+                                    <div>
+                                        <p class="font-semibold text-xl text-gray-100">Zion Garden & Game Lodge</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="group relative h-96 w-16 overflow-hidden cursor-pointer transition-all duration-200">
+                        <img class="h-full object-cover "  src="../assets/game-drive.jpg" alt="...">
+                        <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
+                            <div class="absolute inset-x-5 bottom-6">
+                                <div class="flex gap-3 text-white">
+                                    <div>
+                                        <p class="font-semibold text-xl text-gray-100">Zion Garden & Game Lodge</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="group relative h-96 w-16 overflow-hidden cursor-pointer transition-all duration-200">
+                        <img class="h-full object-cover "  src="../assets/kameelperd.jpg" alt="...">
+                        <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
+                            <div class="absolute inset-x-5 bottom-6">
+                                <div class="flex gap-3 text-white">
+                                    <div>
+                                        <p class="font-semibold text-xl text-gray-100">Zion Garden & Game Lodge</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="group relative h-96 w-16 overflow-hidden cursor-pointer transition-all duration-200">
+                        <img class="h-full object-cover "  src="../assets/sunset_4.jpg" alt="...">
+                        <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
+                            <div class="absolute inset-x-5 bottom-6">
+                                <div class="flex gap-3 text-white">
+                                    <div>
+                                        <p class="font-semibold text-xl text-gray-100">Zion Garden & Game Lodge</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="group relative h-96 w-16 overflow-hidden cursor-pointer transition-all duration-200">
+                        <img class="h-full object-cover"  src="../assets/swembad.png" alt="...">
+                        <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
+                            <div class="absolute inset-x-5 bottom-6">
+                                <div class="flex gap-3 text-white">
+                                    <div>
+                                        <p class="font-semibold text-xl text-gray-100">Zion Garden & Game Lodge</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="group relative h-96 w-16 overflow-hidden cursor-pointer transition-all duration-200">
+                        <img class="h-full object-cover"  src="../assets/tuin_1.png" alt="...">
+                        <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
+                            <div class="absolute inset-x-5 bottom-6">
+                                <div class="flex gap-3 text-white">
+                                    <div>
+                                        <p class="font-semibold text-xl text-gray-100">Zion Garden & Game Lodge</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="group relative h-96 w-16 overflow-hidden cursor-pointer transition-all duration-200">
+                        <img class="h-full object-cover"  src="../assets/zebra.jpg" alt="...">
+                        <div class="invisible absolute inset-0 bg-gradient-to-b from-green-500/20 to-black group-hover:visible">
+                            <div class="absolute inset-x-5 bottom-6">
+                                <div class="flex gap-3 text-white">
+                                    <div>
+                                        <p class="font-semibold text-xl text-gray-100">Zion Garden & Game Lodge</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- <img class="md:h-[30svh] h-[23svh] rounde-[7%] mx-auto rounded-[7%]"  src="../assets/lugfoto.jpg" alt="..."> -->
+            <p class="font-title mx-auto text-center mt-2 p-6 md:text-xl text-[8px]">
                 “For the LORD has chosen Zion, he has desired it for his dwelling, saying, <br/> This is my resting place for ever and ever; here I will sit enthroned, for I have desired it.” Psalm 132:13
             </p>
         </div>
